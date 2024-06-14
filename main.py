cfa9fea851a3d686f55bc10747109bc70061686a
MSWSal <sahansalgado10@gmail.com>
2024-06-14 09:57:09 +0530

    updated to pygame

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from gtts import gTTS
-import playsound
+import pygame
 
 def t2speech(text):
     tts = gTTS(text=text,lang='en',slow=False)
     tts.save("Out.mp3")
-    playsound.playsound("Out.mp3")
+
+    pygame.mixer.init()
+    pygame.mixer.music.load("Out.mp3")
+    pygame.mixer.music.play()
+
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
 
 text = "Hello how are you?"
 t2speech(text)
